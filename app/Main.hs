b05ab6d089677ff836f87d00fd37c2ccf6e56696
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-09-12 15:08:54 +0200

    Cleanup 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,11 @@
+{-# OPTIONS -fplugin=AsyncRattus.Plugin #-}
 module Main where
+
+import AsyncRattus ( box )
+import AsyncRattus.Signal
+
+sums :: Sig Int -> Sig Int
+sums = scan (box (+)) 0
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
