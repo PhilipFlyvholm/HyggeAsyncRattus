c7d605212fe0cf73ba363c0765a61510d97d9d9b
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-09-18 12:31:42 +0200

    Made the most minimal working project (counts up every second)

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,29 @@
 {-# OPTIONS -fplugin=AsyncRattus.Plugin #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Avoid lambda" #-}
 module Main where
 
-import AsyncRattus ( box )
+import AsyncRattus
+import AsyncRattus.Channels
 import AsyncRattus.Signal
+import Prelude hiding (init)
 
-sums :: Sig Int -> Sig Int
-sums = scan (box (+)) 0
+everySecond :: Box (O ())
+everySecond = timer 1000000 -- 1 second = 1.000.000 microseconds
+
+everySecondSig :: Sig ()
+everySecondSig =  () ::: mkSig everySecond
+
+setPrint :: (Producer p a, Show a) => p -> IO ()
+setPrint sig = setOutput sig print
+
+nats :: Int -> Sig Int
+nats init = scan (box (\ n _ -> n+1)) init everySecondSig
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    showNat <- trigger (box (\_ n -> n)) everySecondSig (nats 0)
+
+    setPrint showNat
+    startEventLoop
+
