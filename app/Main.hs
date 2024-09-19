b7d8ee5b91d5064615d9fc32ee7bf93b9847072b
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-09-19 14:41:08 +0200

    fix: update to use SigMaybe

--- app/Main.hs
+++ app/Main.hs
@@ -18,17 +18,17 @@ setPrint :: (Producer p a, Show a) => p -> IO ()
 setPrint sig = setOutput sig print
 
 -- Scan http://www.zvon.org/other/haskell/Outputprelude/scanl_f.html
-nats :: Int -> Sig Int
-nats init = scan (box (\ n _ -> n+1)) init everySecondSig
+nats :: Int -> SigMaybe Int
+nats init = SigMaybe (scan (box (\ _ _ -> Just' 1)) (Just' init) everySecondSig)
 
 
 newtype SigMaybe a = SigMaybe (Sig (Maybe' a))
- 
+
 unSigMaybe :: SigMaybe a -> Sig (Maybe' a)
 unSigMaybe (SigMaybe x) = x
 
 mMap :: Box (a -> b) -> SigMaybe a -> SigMaybe b
-mMap f (SigMaybe (s ::: xs)) = 
+mMap f (SigMaybe (s ::: xs)) =
     case s of
         Nothing' -> SigMaybe (Nothing' ::: delay (unSigMaybe (mMap f (SigMaybe (adv xs)))))
         Just' a ->  SigMaybe (Just' (unbox f a) ::: delay (unSigMaybe (mMap f (SigMaybe (adv xs)))))
@@ -40,9 +40,17 @@ mScan f acc (SigMaybe (x ::: xs)) =
         Just' a  -> SigMaybe (Just' acc' ::: delay (unSigMaybe (mScan f acc (SigMaybe (adv xs)))))
             where acc' = unbox f acc a
 
+--mFilter :: Box (a -> Bool) -> SigMaybe a -> SigMaybe a
+--mFilter f (SigMaybe (s ::: xs)) = 
+  --  cas
+
+--mFilter :: Box (a -> Bool) -> SigMaybe a -> SigMaybe a
+--mFilter f (SigMaybe (s ::: xs)) = 
+  --  cas
+
 main :: IO ()
 main = do
-    showNat <- trigger (box (\_ n -> n)) everySecondSig (nats 0)
+    showNat <- trigger (box (\_ n -> n)) everySecondSig (unSigMaybe (mMap (box (+ 1)) (nats 0)))
 
     setPrint showNat
     startEventLoop
