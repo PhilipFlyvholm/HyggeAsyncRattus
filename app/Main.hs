cec354dc354406f843c27dd2892a75a2170c0eed
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-09-19 11:06:12 +0200

    Added comments

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ import AsyncRattus.Channels
 import AsyncRattus.Signal
 import Prelude hiding (init)
 
-everySecond :: Box (O ())
+everySecond :: Box (O ()) -- Box makes the function stable and O() is a delayed unit
 everySecond = timer 1000000 -- 1 second = 1.000.000 microseconds
 
 everySecondSig :: Sig ()
@@ -17,6 +17,7 @@ everySecondSig =  () ::: mkSig everySecond
 setPrint :: (Producer p a, Show a) => p -> IO ()
 setPrint sig = setOutput sig print
 
+-- Scan http://www.zvon.org/other/haskell/Outputprelude/scanl_f.html
 nats :: Int -> Sig Int
 nats init = scan (box (\ n _ -> n+1)) init everySecondSig
 
