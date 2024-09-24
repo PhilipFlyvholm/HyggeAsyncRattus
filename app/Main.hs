6c4f6837f7ac4dd0f179a7efcb176a6b4d7d979f
Faurby <lasse_fk@hotmail.com>
2024-09-24 11:00:02 +0200

    fix: SigMaybe is correct type now, clean mMap function

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,13 @@
+{-# HLINT ignore "Avoid lambda" #-}
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS -fplugin=AsyncRattus.Plugin #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-{-# HLINT ignore "Avoid lambda" #-}
 module Main where
 
 import AsyncRattus
 import AsyncRattus.Channels
 import AsyncRattus.Signal
-import Control.Monad (mfilter)
 import Prelude hiding (init)
 
 everySecond :: Box (O ()) -- Box makes the function stable and O() is a delayed unit
@@ -21,36 +21,37 @@ setPrint sig = setOutput sig print
 
 -- Scan http://www.zvon.org/other/haskell/Outputprelude/scanl_f.html
 nats :: Int -> SigMaybe Int
-nats init = SigMaybe (scan (box (\_ _ -> Just' 1)) (Just' init) everySecondSig)
+nats init = scan (box (\_ _ -> Just' 1)) (Just' init) everySecondSig
 
-newtype SigMaybe a = SigMaybe (Sig (Maybe' a))
-
-unSigMaybe :: SigMaybe a -> Sig (Maybe' a)
-unSigMaybe (SigMaybe x) = x
+type SigMaybe a = (Sig (Maybe' a))
 
 mMap :: Box (a -> b) -> SigMaybe a -> SigMaybe b
-mMap f (SigMaybe (s ::: xs)) =
-  case s of
-    Nothing' -> SigMaybe (Nothing' ::: delay (unSigMaybe (mMap f (SigMaybe (adv xs)))))
-    Just' a -> SigMaybe (Just' (unbox f a) ::: delay (unSigMaybe (mMap f (SigMaybe (adv xs)))))
+mMap f =
+  AsyncRattus.Signal.map
+    ( box
+        ( \case
+            Just' x -> Just' (unbox f x)
+            Nothing' -> Nothing'
+        )
+    )
 
 mScan :: (Stable b) => Box (b -> a -> b) -> b -> SigMaybe a -> SigMaybe b
-mScan f acc (SigMaybe (x ::: xs)) =
+mScan f acc (x ::: xs) =
   case x of
-    Nothing' -> SigMaybe (Nothing' ::: delay (unSigMaybe (mScan f acc (SigMaybe (adv xs)))))
-    Just' a -> SigMaybe (Just' acc' ::: delay (unSigMaybe (mScan f acc (SigMaybe (adv xs)))))
+    Nothing' -> Nothing' ::: delay (mScan f acc (adv xs))
+    Just' a -> Just' acc' ::: delay (mScan f acc (adv xs))
       where
         acc' = unbox f acc a
 
 mFilter :: Box (a -> Bool) -> SigMaybe a -> SigMaybe a
-mFilter f (SigMaybe (s ::: xs)) =
+mFilter f (s ::: xs) =
   case s of
-    Just' a -> SigMaybe ((if unbox f a then Just' a else Nothing') ::: delay (unSigMaybe (mFilter f (SigMaybe (adv xs)))))
-    Nothing' -> SigMaybe (Nothing' ::: delay (unSigMaybe (mFilter f (SigMaybe (adv xs)))))
+    Just' a -> (if unbox f a then Just' a else Nothing') ::: delay (mFilter f (adv xs))
+    Nothing' -> Nothing' ::: delay (mFilter f (adv xs))
 
 main :: IO ()
 main = do
-  showNat <- trigger (box (\_ n -> n)) everySecondSig (unSigMaybe (mMap (box (+ 1)) (nats 0)))
+  showNat <- trigger (box (\_ n -> n)) everySecondSig (mMap (box (+ 1)) (nats 0))
 
   setPrint showNat
   startEventLoop
