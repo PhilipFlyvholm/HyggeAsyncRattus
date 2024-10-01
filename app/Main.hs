12be788a04e49a2d889f6d3fda7fa327fae40e85
Faurby <lasse_fk@hotmail.com>
2024-10-01 12:51:32 +0200

    feat: add initial mapB for behaviour and tried to do filter, but not working.

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,20 @@
+{-# HLINT ignore "Redundant bracket" #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# HLINT ignore "Avoid lambda" #-}
 {-# OPTIONS -fplugin=AsyncRattus.Plugin #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Redundant bracket" #-}
 
 module Main where
 
 import AsyncRattus
 import AsyncRattus.Channels
 import AsyncRattus.Signal
-import Prelude hiding (init)
 import SigMaybe
-import Data.Time
+import Prelude hiding (init)
 
-type Time = Data.Time.UTCTime
+type Time = Integer
 
-type Ô a = O(a, Time)
+type Ô a = O (a, Time)
 
 data Fun t a = K a | Fun (t -> a)
 
@@ -22,7 +22,31 @@ apply :: Fun t a -> (t -> a)
 apply (K a) = Prelude.const a
 apply (Fun f) = f
 
-data Behaviour a = (Fun Time a) :+: Ô(Behaviour a)
+data Behaviour a = (Fun Time a) :+: Ô (Behaviour a)
+
+-- adv' :: Ô a -> InputValue -> a
+-- adv' (Delay f) inp = f inp
+
+mapB :: Box (a -> b) -> Behaviour a -> Behaviour b
+mapB f (K a :+: xs) = K (unbox f a) :+: delay (let (b', t) = adv xs in (mapB f b', t))
+mapB f (Fun t :+: xs) = Fun (\t' -> unbox f (t t')) :+: delay (let (b', t') = adv xs in (mapB f b', t'))
+
+-- mFilter :: Box (a -> Bool) -> SigMaybe a -> SigMaybe a
+-- mFilter f (Just' a ::: xs) = (if unbox f a then Just' a else Nothing') ::: delay (mFilter f (adv xs))
+-- mFilter f (Nothing' ::: xs) = Nothing' ::: delay (mFilter f (adv xs))
+
+filterB :: Box (a -> Bool) -> Behaviour a -> Behaviour a
+filterB f (K a :+: xs) =
+  if unbox f a
+    then K a :+: delay (let (b', t) = adv xs in (filterB f b', t))
+    else delay (let (b', t) = adv xs in (filterB f b'))
+filterB f (Fun t :+: xs) =
+  Fun(\t' ->
+      if unbox f (t t')
+        then Fun t :+: delay (let (b', t') = adv xs in (filterB f b', t'))
+        else filterB f xs -- <-- this doesnt work... We need call filter on the next element.
+  )
+  :+: xs
 
 main :: IO ()
 main = do
