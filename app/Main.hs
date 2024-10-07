4e8f07a23840573dcfd2cf3702a581206ce5b626
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-10-07 13:06:04 +0200

    Feat: zipWithB V1 (draft) (not final) (final)

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 {-# HLINT ignore "Avoid lambda" #-}
 {-# OPTIONS -fplugin=AsyncRattus.Plugin #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Main where
 
@@ -11,25 +12,27 @@ import AsyncRattus.Channels
 import AsyncRattus.Signal
 import SigMaybe
 import Prelude hiding (init)
+import AsyncRattus.Strict
 
 type Time = Integer
 
-type Ô a = O (a, Time)
+type Ô a = O (a :* Time)
 
-data Fun t a = K a | Fun (t -> a)
+data Fun t a = K !a | Fun !(Box (t -> a))
 
 apply :: Fun t a -> (t -> a)
 apply (K a) = Prelude.const a
-apply (Fun f) = f
+apply (Fun f) = unbox f
 
 data Behaviour a = (Fun Time a) :+: Ô (Behaviour a)
+
 
 -- adv' :: Ô a -> InputValue -> a
 -- adv' (Delay f) inp = f inp
 
 mapB :: Box (a -> b) -> Behaviour a -> Behaviour b
-mapB f (K a :+: xs) = K (unbox f a) :+: delay (let (b', t) = adv xs in (mapB f b', t))
-mapB f (Fun t :+: xs) = Fun (\t' -> unbox f (t t')) :+: delay (let (b', t') = adv xs in (mapB f b', t'))
+mapB f ((K a) :+: xs) = (K (unbox f a)) :+: delay (let (b' :* t) = adv xs in (mapB f b' :* t))
+mapB f ((Fun t) :+: xs) = Fun (box (\t' -> unbox f (unbox t t'))) :+: delay (let (b' :* t'') = adv xs in (mapB f b' :* t''))
 
 -- mFilter :: Box (a -> Bool) -> SigMaybe a -> SigMaybe a
 -- mFilter f (Just' a ::: xs) = (if unbox f a then Just' a else Nothing') ::: de  lay (mFilter f (adv xs))
@@ -46,13 +49,17 @@ zipWith f (a ::: as) (b ::: bs) = unbox f a b ::: delay (
       Both as' bs' -> zipWith f as' bs'
   ) -}
 
-{- zipWithB :: (Stable a, Stable b) => Box (a -> b -> c) -> Behaviour a -> Behaviour b -> Behaviour c
-zipWithB f (K a :+: as) (K b :+: bs) = K (unbox f a b) :+: delay (
+
+zipWithB :: (Stable a, Stable b) => Box (a -> b -> c) -> Behaviour a -> Behaviour b -> Behaviour c
+zipWithB f (a :+: as) (b :+: bs) = 
+  app a b :+: delay (
     case select as bs of
-      Fst (as', ast') lbs -> let (lbs', lbst') = adv lbs' in zipWithB f as' (b :+: lbs')
-      Snd las (bs', bst') -> let (las', last') = adv las' in zipWithB f (a :+: las') bs'
-      Both (as', ast') (bs', bst') -> let (as'', ast'') = adv as' in let (bs'', bst'') = adv bs' in zipWithB f as'' bs''
-  ) -}
+      Fst (as' :* ast) lbs -> zipWithB f as' (b :+: lbs) :* ast
+      Snd las (bs' :* bst) -> zipWithB f (a :+: las) bs' :* bst
+      Both (as' :* ast) (bs' :* bst) -> zipWithB f as' bs' :* (max ast bst)
+  )
+  where app (K a') (K b') = K (unbox f a' b')
+        app (Fun a') (Fun b') = Fun (box (\t -> unbox f (unbox a' t) (unbox b' t)))
 
 
 
