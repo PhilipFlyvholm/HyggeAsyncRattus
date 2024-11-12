923a2127e8dc39cef3978ea76ca5c73973cc20b0
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-11-12 11:27:56 +0100

    Chore: Clean up

--- app/ZippedWithTest.hs
+++ app/ZippedWithTest.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS -fplugin=AsyncRattus.Plugin #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 
 module ZippedWithTest where
 
@@ -17,14 +18,16 @@ import Prelude hiding (init)
 gen :: StdGen
 gen = mkStdGen 137
 
+printSampled :: (Show a) => [Time :* a] -> IO()
+printSampled = mapM_ (putStrLn . (\(t :* a) -> show t ++ " = " ++ show a))
+
 zippedWithTest :: IO ()
 zippedWithTest = do
   let rawBeh = beh 1
   let zippedBeh = Behaviour.zipWith (box (+)) rawBeh (beh' 1)
-  
+
   currentTime <- getCurrentStrictTime
   print ("Current time: " ++ show currentTime)
   print "Sampled behaviour:"
-  let sampled = Prelude.map (\(t :* a) -> show t ++ " = " ++ show a) (sample 5 5 zippedBeh gen)
-  mapM_ putStrLn sampled
+  printSampled (sample 5 5 zippedBeh gen)
   startEventLoop
