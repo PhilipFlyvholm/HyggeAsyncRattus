ff35150f36f21b2f0b13d28b2c307a4aa6099df5
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-11-12 09:37:36 +0100

    Feat: Invalid command event

--- app/Console.hs
+++ app/Console.hs
@@ -33,7 +33,7 @@ setQuit :: (Producer p a) => p -> IO ()
 setQuit event = setOutput event (const exitSuccess)
 
 getJustValues :: IO (OT (Event (Maybe' a))) -> IO (OT (Event a))
-getJustValues e = do 
+getJustValues e = do
     e' <- e
     unbox <$> filterMapAwait (box id) e'
 
@@ -43,6 +43,9 @@ startConsole = do
   quitEvent :: OT (Event Text) <- unbox <$> filterAwait (box (== "quit")) console
   showEvent :: OT (Event Text) <- unbox <$> filterAwait (box (== "show")) console
   resetEvent :: OT (Event Text) <- unbox <$> filterAwait (box (== "reset")) console
+  
+  otherEvent :: OT (Event Text) <- unbox <$> filterAwait (box (\s -> s /= "reset" && s /= "show" && s /= "reset")) console
+  showHelp :: OT (Event Text) <- getJustValues (unbox <$> triggerAwaitIO (box (\_ t -> t)) otherEvent (K "Commands: show, reset, quit" :+: never))
 
   startTimer :: Behaviour Int <- Behaviour.startTimerBehaviour
   lastReset :: Behaviour Int <- do
@@ -50,11 +53,12 @@ startConsole = do
         let beh = mkBehaviourAwait n
         return (switch (K 0 :+: never) beh)
   let currentTimer :: Behaviour Int = Behaviour.zipWith (box (-)) startTimer lastReset
-  
+
 
   showTimer :: OT (Event Int) <- getJustValues (unbox <$> triggerAwaitIO (box (\_ n -> n)) showEvent currentTimer)
- 
+
   setPrint showTimer
+  setPrint showHelp
 
   setQuit quitEvent
   startEventLoop
